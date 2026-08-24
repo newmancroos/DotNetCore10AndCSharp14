9efd44ee596cee71077803cef19e06bd3f16e3da
newancroos <newmancroos@gmail.com>
2026-08-24 18:14:26 -0400

    GRPC

---
+++ grpcGreeter/GrpcGreeterClient/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7133/");
+var client = new  Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Greet Client" });
+Console.WriteLine("Greeting: " + reply.Message);
+
+Console.ReadKey();
